f96a5d8e27e50494af9613d9e5fa2f21d9f5bdb5
Josh Starcher <josh.starcher@gmail.com>
2014-03-18 14:05:24 -0400

    Add method for sshing into an opsworks server.

---
+++ bin/instance_ip.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'aws-sdk'
+
+stack_name = ARGV.shift
+
+AWS.config({
+  :access_key_id => ENV['AWS_KEY_ID'],
+  :secret_access_key => ENV['AWS_SECRET_KEY'],
+})
+
+opsworks = AWS::OpsWorks.new
+c = opsworks.client
+stacks = c.describe_stacks[:stacks]
+
+stack = stacks.detect {|s| s[:name].downcase == stack_name.downcase}
+
+instances = c.describe_instances(stack_id: stack[:stack_id])[:instances]
+instance = instances.detect {|i| i[:status] == 'online'}
+
+puts instance[:private_ip]
