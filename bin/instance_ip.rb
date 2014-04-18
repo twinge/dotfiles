1065315a61af8941ad52f2054a59f87b8b250940
Josh Starcher <josh.starcher@gmail.com>
2014-04-18 10:59:07 -0400

    Allow sshing directly to an IP

--- bin/instance_ip.rb
+++ bin/instance_ip.rb
@@ -5,27 +5,32 @@ require 'aws-sdk'
 
 stack_name = ARGV.shift
 
-stack_name = case stack_name
-when 'prod'
-  'Rails Production - Misc'
-when 'stage'
-  'Rails Staging'
+if stack_name.include?('.')
+  # ip address
+  puts stack_name
 else
-  stack_name
+  stack_name = case stack_name
+               when 'prod'
+                 'Rails Production - Misc'
+               when 'stage'
+                 'Rails Staging'
+               else
+                 stack_name
+               end
+
+  AWS.config({
+    :access_key_id => ENV['AWS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_KEY'],
+  })
+
+  opsworks = AWS::OpsWorks.new
+  c = opsworks.client
+  stacks = c.describe_stacks[:stacks]
+
+  stack = stacks.detect {|s| s[:name].downcase == stack_name.downcase}
+
+  instances = c.describe_instances(stack_id: stack[:stack_id])[:instances]
+  instance = instances.detect {|i| i[:status] == 'online'}
+
+  puts instance[:private_ip]
 end
-
-AWS.config({
-  :access_key_id => ENV['AWS_KEY_ID'],
-  :secret_access_key => ENV['AWS_SECRET_KEY'],
-})
-
-opsworks = AWS::OpsWorks.new
-c = opsworks.client
-stacks = c.describe_stacks[:stacks]
-
-stack = stacks.detect {|s| s[:name].downcase == stack_name.downcase}
-
-instances = c.describe_instances(stack_id: stack[:stack_id])[:instances]
-instance = instances.detect {|i| i[:status] == 'online'}
-
-puts instance[:private_ip]
